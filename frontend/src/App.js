04d42223d906b032c7e15d66f887a6bd0b02019c
Mason Turner <turner_m@lynchburg.edu>
2023-02-14 13:44:09 -0500

    React setup

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Welcome to Clay Market</h1>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
